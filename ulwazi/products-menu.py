d9f528c20af5f5aee11f51d1496647bc82311f3e
Vladimir <48120135+izmalk@users.noreply.github.com>
2025-04-01 18:07:11 +0100

    Refactor the parser code

--- ulwazi/products-menu.py
+++ ulwazi/products-menu.py
@@ -1,29 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://canonical.com/"
+PRODUCT_MENU_PARSING_URL = "https://canonical.com/"
 
 
-def fetch_and_parse(url):
+def fetch_and_parse(url: str) -> BeautifulSoup:
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for bad responses (4xx and 5xx)
-        
-        soup = BeautifulSoup(response.text, 'html.parser')
-        return soup
+        return BeautifulSoup(response.text, 'html.parser')
     except requests.exceptions.RequestException as e:
         print(f"Error fetching the URL: {e}")
         return None
 
-if __name__ == "__main__":
-    address = URL
-    parsed_html = fetch_and_parse(address)
-    navigation_menu = parsed_html.find("header", id="navigation")
+def get_nav_menu(page: BeautifulSoup) -> str:
+    return page.find("header", id="navigation").prettify()
 
-    if navigation_menu:
-        # print("Found the NavMenu: \n", navigation_menu.prettify())
-        num_lines = len(navigation_menu.prettify().splitlines())
-        print(f"Parsed the NavMenu spanning {num_lines} lines.")
+if __name__ == "__main__":
+    address = PRODUCT_MENU_PARSING_URL
+    parsed_html = fetch_and_parse(address)
+    navigation_menu = get_nav_menu(parsed_html)
+    num_lines = len(navigation_menu.splitlines())
+    if num_lines > 100:
+        print(f"The parsed the NavMenu has {num_lines} lines.")
     else:
         print("NavMenu not found.")
+        exit(1)
 
+    print("First 1000 symbols of the NavMenu: \n" + navigation_menu[:1000])
