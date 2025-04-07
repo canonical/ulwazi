af2a025ef0d5abf4240ecf6c7f647c1cfdd3dad0
Vladimir <48120135+izmalk@users.noreply.github.com>
2025-04-07 15:24:36 +0100

    Product menu initial render

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,31 @@ html_context = {
     'company': 'Canonical',
     'link1_URL': 'https://snapcraft.io/',
     'link1_name': 'Snapcraft',
+    'product_menu_enabled': True,  # Enable Product menu
 }
 
 html_favicon = '_static/favicon.png'
+
+# import product_menu as p_menu
+
+# p_menu.save2file(p_menu.get_nav_menu(p_menu.fetch_and_parse(p_menu.PRODUCT_MENU_PARSING_URL)))
+
+# def update_product_menu():
+#     """Fetches, parses, and saves the product navigation menu."""
+#     try:
+#         # Fetch a page with the menu from the URL
+#         raw_page= p_menu.fetch_and_parse(p_menu.PRODUCT_MENU_PARSING_URL)
+
+#         # Crop the menu
+#         nav_menu = p_menu.get_nav_menu(raw_page)
+
+#         # Save it to a file
+#         p_menu.save2file(nav_menu)
+
+#         print("Product menu updated successfully.")
+
+#     except Exception as e:
+#         print(f"Failed to update product menu: {e}")
+
+# # Execute the update
+# update_product_menu()
--- docs/product_menu.py
+++ docs/product_menu.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 PRODUCT_MENU_PARSING_URL = "https://canonical.com/"
-
+PRODUCT_MENU_FILENAME = "product_menu_copy.html"
+PRODUCT_MENU_DIRECTORY = "docs/_static"
 
 def fetch_and_parse(url: str) -> BeautifulSoup:
     try:
@@ -16,6 +17,11 @@ def fetch_and_parse(url: str) -> BeautifulSoup:
 def get_nav_menu(page: BeautifulSoup) -> str:
     return page.find("header", id="navigation").prettify()
 
+def save2file(content:str) -> None:
+    filename = PRODUCT_MENU_DIRECTORY.strip("/") + "/" + PRODUCT_MENU_FILENAME
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(content)
+
 if __name__ == "__main__":
     address = PRODUCT_MENU_PARSING_URL
     parsed_html = fetch_and_parse(address)
@@ -28,3 +34,6 @@ if __name__ == "__main__":
         exit(1)
 
     print("First 1000 symbols of the NavMenu: \n" + navigation_menu[:1000])
+    save2file(navigation_menu)
+    print(f"Saved product menu as {PRODUCT_MENU_DIRECTORY.strip("/")}/{PRODUCT_MENU_FILENAME}")
+    
