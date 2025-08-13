64a4844fb95a2dc61b22b7d06441bec6d46a0bf5
Vladimir <48120135+izmalk@users.noreply.github.com>
2025-08-13 09:31:32 +0100

    Improve HTML generation script resilience

--- docs/product_menu.py
+++ docs/product_menu.py
@@ -1,9 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import logging
 
 PRODUCT_MENU_PARSING_URL = "https://canonical.com/"
 PRODUCT_MENU_FILENAME = "product_menu_copy.html"
 PRODUCT_MENU_DIRECTORY = "docs/_static"
+
+logging.basicConfig(level=logging.INFO)  # change to logging.DEBUG to enable debug messages
+logger = logging.getLogger(__name__)
 
 def fetch_and_parse(url: str) -> BeautifulSoup:
     try:
@@ -11,7 +16,7 @@ def fetch_and_parse(url: str) -> BeautifulSoup:
         response.raise_for_status()  # Raise an error for bad responses (4xx and 5xx)
         return BeautifulSoup(response.text, 'html.parser')
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching the URL: {e}")
+        logger.error(f"Error fetching the URL: {e}")
         return None
 
 def get_nav_menu(page: BeautifulSoup) -> str:
@@ -19,6 +24,7 @@ def get_nav_menu(page: BeautifulSoup) -> str:
 
 def save2file(content:str) -> None:
     filename = PRODUCT_MENU_DIRECTORY.strip("/") + "/" + PRODUCT_MENU_FILENAME
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, "w", encoding="utf-8") as file:
         file.write(content)
 
@@ -28,12 +34,12 @@ if __name__ == "__main__":
     navigation_menu = get_nav_menu(parsed_html)
     num_lines = len(navigation_menu.splitlines())
     if num_lines > 100:
-        print(f"The parsed the NavMenu has {num_lines} lines.")
+        logger.debug(f"The parsed the NavMenu has {num_lines} lines.")
     else:
-        print("NavMenu not found.")
+        logger.error("NavMenu not found.")
         exit(1)
 
-    print("First 1000 symbols of the NavMenu: \n" + navigation_menu[:1000])
+    logger.debug("First 1000 symbols of the NavMenu: \n" + navigation_menu[:1000])
     save2file(navigation_menu)
-    print(f"Saved product menu as {PRODUCT_MENU_DIRECTORY.strip("/")}/{PRODUCT_MENU_FILENAME}")
+    logger.info(f"Saved product menu as {PRODUCT_MENU_DIRECTORY.strip("/")}/{PRODUCT_MENU_FILENAME}")
     
