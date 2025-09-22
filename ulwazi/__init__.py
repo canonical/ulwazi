9b1fac529a23a62b3e81ad87f26a313f41a44fcf
Vladimir <48120135+izmalk@users.noreply.github.com>
2025-09-22 20:12:41 +0100

    Remove page title and redundant margins

--- ulwazi/__init__.py
+++ ulwazi/__init__.py
@@ -199,15 +199,29 @@ def modify_local_toc(toc:str) -> str:
     if not toc:
         return toc
     toc_html = BeautifulSoup(toc, "html.parser")
+    # Remove a redundant <ul>
     top_ul = toc_html.find("ul")
     if top_ul:
-        top_ul.unwrap() # Remove the outermost <ul>
+        top_ul.unwrap()
+    # Remove the page title <li>
+    top_li = toc_html.find("li")
+    if top_li:
+        top_li.unwrap()
+    # Remove the link to the page title <a>
+    top_a = toc_html.find("a")
+    if top_a:
+        top_a.decompose()
+    # Remove a redundant margin for headings
+    top_ul = toc_html.find("ul")
+    if top_ul:
+        top_ul.unwrap()
+    # Assign classes from Vanilla Framework
     for li in toc_html.find_all("li"):
         li["class"] = ["p-table-of-contents__item"]
         a = li.find("a", recursive=False)
         if a:
             a["class"] = ["p-table-of-contents__link"]
-    
+
     return str(toc_html)
 
 def _html_page_context(
