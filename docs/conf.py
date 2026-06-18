4a3f10d3fab7705c1ef05ba29cce8a8c13d1f03d
Vladimir Izmalkov <48120135+izmalk@users.noreply.github.com>
2026-06-18 14:38:34 +0100

    Hide product menu and Horizontal menu buttons

--- docs/conf.py
+++ docs/conf.py
@@ -193,14 +193,14 @@ html_context = {
 
     # Horizontal Nav Menu
     "company": "Canonical",
-    "link1_URL": "https://snapcraft.io/",
-    "link1_name": "First optional link",
-    "link2_URL": "https://snapcraft.io/",
-    "link2_name": "Second optional link",
+    # "link1_URL": "https://snapcraft.io/",
+    # "link1_name": "First optional link",
+    # "link2_URL": "https://snapcraft.io/",
+    # "link2_name": "Second optional link",
 
     # Canonical Product menu
     # Uncomment if you need a product menu added on the top of every page
-    "add_product_menu": True,
+    "add_product_menu": False,
     
     # Main Horizontal menu
     # "is_docs": False, # Purpose unknown
