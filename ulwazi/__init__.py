093f077e461a09e75b6a7764293a98d93ddedb75
MohamedSefyaniLakrizi <mohamed.sefyanilakrizi@canonical.com>
2026-09-14 15:41:56 +0100

    feat: change google tag through the tag_id variable

--- ulwazi/__init__.py
+++ ulwazi/__init__.py
@@ -96,6 +96,7 @@ def config_inited(app: Sphinx, config: Config) -> None:
         ("sequential_nav", "none"),
         ("display_contributors", True),
         ("path", "/docs"),
+        ("tag_id", "GTM-N384JMX2"),
     ]
 
     for value, default in values_and_defaults:
